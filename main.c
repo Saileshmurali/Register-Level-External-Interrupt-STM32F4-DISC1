bad52d73f25e443f40d8dbf220e949be08d20ea6
Sailesh M <43719932+Saileshmurali@users.noreply.github.com>
2021-04-01 19:29:40 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,18 +10,18 @@ void EXT_Init(void)
 
 //1. Enable TIM@ and GPIO clock
 	RCC->APB1ENR |= (1<<0);  // enable PORTA clock
-	EXTI->RTSR |=(1<<0);
-	EXTI->IMR |=(1<<0);
+	EXTI->RTSR |=(1<<0);     // enable rising edge trigger
+	EXTI->IMR |=(1<<0);      // disable the interrupt mask
 }
 void configureLED(void)
 {
 	RCC->AHB1ENR |=(1UL<<3);  //Enable GPIOD clock
 	GPIOD->MODER &= ~(0xFFUL<<12*2);
-	GPIOD->MODER |= (0x55UL<<12*2);
+	GPIOD->MODER |= (0x55UL<<12*2);    //configure pins PD12-15 as output
 }
 void External_Interrupt_Enable(void)
 {
-	NVIC->ISER[0] |= 1<<6;
+	NVIC->ISER[0] |= 1<<6;    //enable external interrupt line
 
 }
 int main ()
@@ -38,7 +38,7 @@ int main ()
 void EXTI0_IRQHandler( )
 {
 	GPIOD->ODR ^= (0xFUL<<12);
-	EXTI->PR |= (1<<0);
+	EXTI->PR |= (1<<0);          //clear the interrupt flag
 
 }
 
